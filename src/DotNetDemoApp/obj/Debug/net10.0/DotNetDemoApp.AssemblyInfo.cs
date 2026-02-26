a64b03e24118816b3f2dd4d25fc4cbdcddeaf571
Awinash Dosalwar <awinash.dosalwar@globant.com>
2026-02-26 19:58:11 +0530

    restore error

--- src/DotNetDemoApp/obj/Debug/net10.0/DotNetDemoApp.AssemblyInfo.cs
+++ src/DotNetDemoApp/obj/Debug/net10.0/DotNetDemoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDemoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1478799f1c45de16b08dd04ffeffd2f5193c88f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ff22f9cee700d7c5cbb2aa0590fe29f8947ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDemoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDemoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
